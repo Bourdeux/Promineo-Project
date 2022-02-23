6c0b31677a1c5454d6462e029f6a3c637077da4a
Bourdeux <danielteh.eh@gmail.com>
2022-02-22 20:09:06 -0600

    week 1 stuff

---
+++ JavaWeek1/src/week1.java
@@ -0,0 +1,31 @@
+
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Rob's Jeep descriptions
+		double jeepTopPrice = 2000.00;
+		double debitCardBalance = 3000.00;
+		int tireSize = 35;
+		int jeepWeight = 3970;
+		char directionIAmGettingLostIn = 'W';
+		String lastKnownLocation = "Maob, UT";
+		float engineSize = 2.0f;
+		boolean canEat = debitCardBalance > jeepTopPrice; 
+		
+		debitCardBalance -= jeepTopPrice;
+		String price = String.format("%.2f", jeepTopPrice);
+		String balance = String.format("%.f", debitCardBalance);
+		
+		System.out.println();
+		
+		System.out.println(debitCardBalance);
+		
+				
+		
+	
+		
+		
+	}
+
+}
