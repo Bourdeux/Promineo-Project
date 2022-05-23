9171c8ac93285a82e822746ef3d678b9fbdca09a
Bourdeux <danielteh.eh@gmail.com>
2022-05-22 19:22:04 -0500

    fixed Wk13 HW jeepSales.java

--- CodingAssignment/Wk13/JeepSales.java
+++ CodingAssignment/Wk13/JeepSales.java
@@ -1,4 +1,4 @@
-package com.promineo.jeep;
+package com.promineotech.jeep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
